e3064b4b4820d574891d6bfebb5e96e267c4a7ea
uriloop <orlopez@elpuig.xeill.net>
2022-02-19 20:32:02 +0100

    commit per provar que funcionen les proves

--- src/main/java/ex1/Enunciat.java
+++ src/main/java/ex1/Enunciat.java
@@ -2,6 +2,7 @@ package ex1;
 
 public class Enunciat {
 /*
+
     1. Fes les modificacions necessàries perquè aquest projecte funcioni correctament fent servir Maven.
     2. Puja el projecte a GitHub i activa les accions necessàries perquè, en fer commit, s'executin els jocs de proves
        que muntaràs en els següents exercicis. Pots executar qualsevol joc de proves del package que vulguis (o més
